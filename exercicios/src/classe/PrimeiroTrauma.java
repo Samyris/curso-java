d2293b1cde1ec14377db4280ae5e6cf56a520eab
Samyris Alves <61566573+Samyris@users.noreply.github.com>
2022-01-19 17:40:04 -0300

    Update PrimeiroTrauma.java

--- exercicios/src/classe/PrimeiroTrauma.java
+++ exercicios/src/classe/PrimeiroTrauma.java
@@ -2,14 +2,15 @@ package classe;
 
 public class PrimeiroTrauma {
 
-	int a = 3;
-	static int b = 4;
-
+	int a = 3; //deve-se referenciar o ponto a para chama-lo
+	static int b = 7 // ponto estatico pode ser acessado só chamando	
+			
 	public static void main(String[] args) {
-		
 		PrimeiroTrauma p = new PrimeiroTrauma();
+		//para conseguir acessar o ponto a
 		System.out.println(p.a);
 		
 		System.out.println(b);
+		
 	}
 }
